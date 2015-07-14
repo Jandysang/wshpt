03c97509a489014edb888d699513ca83ed0073a8
Jandy.Sang <sangming12@sina.cn>
2015-07-15 01:23:07 +0800

    处理首页

--- js/home.js
+++ js/home.js
@@ -1,4 +1,13 @@
 
 $(document).ready(function(){
 	$(".ui-slideshow-box").slideshow({skip:false});
+
+	
+	$('[data-toggle="popover"]').each(function(){
+		$(this).popover({
+		    html:       true,
+		    trigger:    'hover',
+		    content:    $("<div>").append($(this).parent().find(".h-pro-pop").html()).css({width:242})
+		});
+	});
 });
